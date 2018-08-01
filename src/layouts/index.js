141a30acf99c844b00b901313968740f4025b79f
Mats Ekelund <mats.ekelund@verisure.com>
2018-08-01 09:24:18 +0200

    Added ESLint

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.css'
+import Header from '../components/header';
+import './index.css';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -21,18 +21,17 @@ const Layout = ({ children, data }) => (
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
-      }}
-    >
+      }}>
       {children()}
     </div>
   </div>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
@@ -42,4 +41,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Link from "gatsby-link";
+import React from 'react';
+import Link from 'gatsby-link';
 
 export default ({ data }) => {
   console.log(data);
@@ -13,38 +13,36 @@ export default ({ data }) => {
         <div key={node.id}>
           <Link
             to={node.fields.slug}
-            css={{ textDecoration: `none`, color: `inherit` }}
-          >
+            css={{ textDecoration: `none`, color: `inherit` }}>
             <h3 style={{ marginBottom: '4px' }}>
-              {node.frontmatter.title}{" "}
-              <span style={{ color: "#BBB" }}>— {node.frontmatter.date}</span>
+              {node.frontmatter.title}{' '}
+              <span style={{ color: '#BBB' }}>— {node.frontmatter.date}</span>
             </h3>
           </Link>
-            <p>{node.excerpt}</p>
+          <p>{node.excerpt}</p>
         </div>
-          ))}
+      ))}
     </div>
-      );
-      };
+  );
+};
 
-      export const query = graphql`
+export const query = graphql`
   query IndexQuery {
-        allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-        totalCount
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
       edges {
         node {
-      id
+          id
           frontmatter {
-        title
+            title
             date(formatString: "DD MMMM, YYYY")
-    }
+          }
           fields {
-        slug
+            slug
+          }
+          excerpt
+        }
       }
-      excerpt
     }
   }
-}
-}
 `;
-
